bfd220daad73c43a597a85078f5f0e977cb7e9b6
Dan <Dan@Dinkydetails.com>
2014-02-12 01:11:27 -0500

    imageview set

--- WakeTimerforKids/gen/com/dinkydetails/waketimerforkids/R.java
+++ WakeTimerforKids/gen/com/dinkydetails/waketimerforkids/R.java
@@ -27,12 +27,14 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int sun=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090003;
-        public static final int largeClockText=0x7f090002;
-        public static final int smallClockText=0x7f090001;
-        public static final int textclock1=0x7f090000;
+        public static final int action_settings=0x7f090004;
+        public static final int dayNightImage=0x7f090000;
+        public static final int largeClockText=0x7f090003;
+        public static final int smallClockText=0x7f090002;
+        public static final int textclock1=0x7f090001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
