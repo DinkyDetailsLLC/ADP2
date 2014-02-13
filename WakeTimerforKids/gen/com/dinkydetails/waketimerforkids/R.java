daaa31fcaf23e494a6fb67aeeaa75600837694f9
Dan <Dan@Dinkydetails.com>
2014-02-12 21:33:42 -0500

    Revamped the UI. Activities are passing

--- WakeTimerforKids/gen/com/dinkydetails/waketimerforkids/R.java
+++ WakeTimerforKids/gen/com/dinkydetails/waketimerforkids/R.java
@@ -26,25 +26,69 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int sun=0x7f020001;
+        public static final int blue=0x7f020000;
+        public static final int camera=0x7f020001;
+        public static final int clock=0x7f020002;
+        public static final int green=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int orange=0x7f020005;
+        public static final int pink=0x7f020006;
+        public static final int purple=0x7f020007;
+        public static final int red=0x7f020008;
+        public static final int save_button=0x7f020009;
+        public static final int settings=0x7f02000a;
+        public static final int settingsmall=0x7f02000b;
+        public static final int sun=0x7f02000c;
+        public static final int switch_on=0x7f02000d;
     }
     public static final class id {
-        public static final int action_settings=0x7f090004;
-        public static final int dayNightImage=0x7f090000;
-        public static final int largeClockText=0x7f090003;
-        public static final int smallClockText=0x7f090002;
-        public static final int textclock1=0x7f090001;
+        public static final int action_settings=0x7f090017;
+        public static final int bottomClocks=0x7f090005;
+        public static final int checkBox1=0x7f090013;
+        public static final int imageView1=0x7f090002;
+        public static final int imageView2=0x7f090006;
+        public static final int imageView3=0x7f090008;
+        public static final int imageView4=0x7f090009;
+        public static final int imageView5=0x7f09000a;
+        public static final int imageView6=0x7f09000b;
+        public static final int linearLayout1=0x7f090000;
+        public static final int saveButton=0x7f090007;
+        public static final int scrollView1=0x7f090011;
+        public static final int textView1=0x7f090001;
+        public static final int textView10=0x7f090015;
+        public static final int textView11=0x7f090016;
+        public static final int textView2=0x7f090004;
+        public static final int textView3=0x7f09000c;
+        public static final int textView4=0x7f09000d;
+        public static final int textView5=0x7f09000e;
+        public static final int textView6=0x7f09000f;
+        public static final int textView7=0x7f090010;
+        public static final int textView8=0x7f090012;
+        public static final int textView9=0x7f090014;
+        public static final int textclock1=0x7f090003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int preferences=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f080000;
     }
     public static final class string {
+        public static final int Clock=0x7f06000a;
+        public static final int CountClock=0x7f06000e;
+        public static final int EnableCountClock=0x7f06000f;
+        public static final int EnableNapMode=0x7f060010;
+        public static final int Kid_Clock=0x7f060011;
+        public static final int NapMode=0x7f06000d;
+        public static final int NightMode=0x7f06000c;
+        public static final int Wakeup=0x7f060009;
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
+        public static final int blur_image=0x7f060008;
+        public static final int childtime=0x7f06000b;
+        public static final int choose_color=0x7f060006;
+        public static final int choose_image=0x7f060007;
         public static final int clock=0x7f060003;
         public static final int hello_world=0x7f060002;
         public static final int largeClockText=0x7f060005;
